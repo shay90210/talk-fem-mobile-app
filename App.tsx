3ec20c881538ac79903e1bd1e4b492e90a9fb9fc
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-25 18:33:36 -0500

    updated app.tsx

--- App.tsx
+++ App.tsx
@@ -1,23 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Main from "./screens/MainComponent";
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>HELLO SHANNON</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
