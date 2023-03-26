19bc268f92647fa6a732ffe3b64390a5e5579088
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-26 16:23:22 -0500

    updated homepage

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from 'react-native';
+import { Text, View, ImageBackground, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
+    const localImage = require('./../assets/mainbackground.jpg')
+
     return (
-        <View>
-            <Text>HOME SCREEN</Text>
+        <View style={styles.container}>
+            <ImageBackground source={localImage} resizeMode='cover' style={styles.image}>
+                <Text>HOME SCREEN</Text>
+            </ImageBackground>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+})
 
 export default HomeScreen;
